2c6bfda6f38b6492df2a7ce526744834c9fa076e
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-06-24 13:03:26 -0300

    0.0.1: Inicialização de servidor express e conexão com cliente

--- public/game.js
+++ public/game.js
@@ -1,1 +1,6 @@
+const socket = io();
+socket.on('connect', () => {
+    console.log('Conectado ao servidor via Socket.IO!');
+});
+
 console.log("Aplicação do cliente iniciado");
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import ip from 'ip';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const server = createServer(app); // criar servidor HTTP para o socket.io
+const io = new Server(server);    // inicializar socket.io
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
@@ -15,6 +20,16 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.listen(PORT, () => {
+// Configurar evento de conexão do socket
+io.on('connection', (socket) => {
+  console.log('Cliente conectado:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
+
+// Usar o `server.listen` em vez de `app.listen`
+server.listen(PORT, () => {
   console.log(`Servidor rodando em http://${ip.address()}:${PORT}`);
 });
