c6889dca06619f06bc59261acfbd45af8f17ab70
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-06-24 12:55:21 -0300

    0.0.1: Corrigindo build do projeto

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../public/index.html')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/ping', (req, res) => {
   res.send('pong');
