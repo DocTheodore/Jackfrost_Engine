51ea1a3bedf67631837db102b1686878812fc41c
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-07-31 18:01:28 -0300

    0.0.1: correções do servidor

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,21 @@
 import express from "express";
+import { fileURLToPath } from "url";  // Importa o módulo de utilidades de URL
+import path from "path";
+
+// Obtém o diretório atual de forma compatível com ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 let port = 3000;
 
-app.get("/", (req,res) => {
-    res.send("Hello World");
-    console.log("Response sent");
+app.use('/build', express.static(path.join(__dirname, 'build')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    console.log("Página carregada");
 });
 
 app.listen(port, () => {
     console.log("App listening on port: ", port);
 });
-
