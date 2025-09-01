5df8cc6b949dc58b888887598700ec784f4ebb11
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-09-01 18:02:19 -0300

    0.0.1: reatividade html

--- src/client/main.ts
+++ src/client/main.ts
@@ -9,8 +9,14 @@ const gametime = GameTime;
 // Funções do HTML
 window.sendNameFromInput = function() {
     const name = (document.getElementById('playerNameBox') as HTMLInputElement).value || '';
+    const idRegister = document.getElementById('login');
+    const myName = document.getElementById('playerName');
     console.log(name, selectedColor);
     __sendLoginData({name: name, color:selectedColor||0});
+    if(myName && idRegister) {
+        myName.innerText = name;
+        idRegister.style.visibility = 'hidden';
+    }
 }
 
 // Funções de loop
